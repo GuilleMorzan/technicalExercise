6162c7d7656349287f262bc3e2a75a106c7e2e27
guillermo.morzan <guillermo.morzan@gmail.com>
2018-12-21 15:35:32 -0300

    This commit contains the invocation from the rental company class to the rental strategy.

--- rentalBusiness/src/main/java/ar/com/fdv/rentalBusiness/domainModel/RentalCompany.java
+++ rentalBusiness/src/main/java/ar/com/fdv/rentalBusiness/domainModel/RentalCompany.java
@@ -1,11 +1,14 @@
 package ar.com.fdv.rentalBusiness.domainModel;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
 import ar.com.fdv.rentalBusiness.businessException.BadRequestException;
+import ar.com.fdv.rentalBusiness.businessException.NotAvailableBikesException;
 import ar.com.fdv.rentalBusiness.dto.CustomerOrder;
+import ar.com.fdv.rentalBusiness.rentalStrategy.RentalStrategy;
 import ar.com.fdv.rentalBusiness.utils.LoggerUtils;
 
 public class RentalCompany {
@@ -40,17 +43,44 @@ public class RentalCompany {
 		this.availableBikes = availableBikes;
 	}
 	
-	public void rent(List<CustomerOrder> customerOrder) throws BadRequestException{
+	public void rent(List<CustomerOrder> customerOrders) throws BadRequestException, NotAvailableBikesException, IOException{
+		List<Bike> rentedBikes = new ArrayList<Bike>();
+		Float totalAmount = 0F;
+		
 		LoggerUtils.configureLogger(LOGGER);
 		LOGGER.info("A customer wants to rent one or more bikes.");
-		validateParameters(customerOrder);
+
+		validateInputParameter(customerOrders);
+		for(CustomerOrder customerOrder: customerOrders){
+			RentalStrategy rentalStrategy = customerOrder.getRentalStrategy();
+			Integer timeQuantity = customerOrder.getTimeQuantity();
+			
+			validateCustomerOrder(customerOrder);
+			Bike bikeToBeRent = getABike();
+			totalAmount = totalAmount + rentalStrategy.rent(bikeToBeRent, timeQuantity);
+			rentedBikes.add(bikeToBeRent);
+		}
 	}
 
-	private void validateParameters(List<CustomerOrder> customerOrder) throws BadRequestException {
-		LoggerUtils.configureLogger(LOGGER);
-		if(customerOrder == null || customerOrder.size() == 0){
-			LOGGER.severe("The customer order is null or empty.");
-			throw new BadRequestException("The customer order is null or empty.");
+	private void validateInputParameter(List<CustomerOrder> customerOrders) throws BadRequestException {
+		if(customerOrders == null || customerOrders.size() == 0){
+			LOGGER.severe("The list of customer orders is null or empty.");
+			throw new BadRequestException("The list of customer orders is null or empty.");
 		}
+	}
+
+	private void validateCustomerOrder(CustomerOrder customerOrder) throws BadRequestException {
+		if(customerOrder.getRentalStrategy() == null || customerOrder.getTimeQuantity() == null){
+			LOGGER.severe("The rental strategy or the time quantity is null.");
+			throw new BadRequestException("The rental strategy or the time quantity is null.");
+		}
+	}
+	
+	private Bike getABike() throws NotAvailableBikesException {
+		if(availableBikes == null || availableBikes.size() == 0){
+			LOGGER.severe("The rental company does not have any bike at this moment.");
+			throw new NotAvailableBikesException("The rental company does not have any bike at this moment.");
+		}
+		return availableBikes.get(0);
 	}
 }
--- rentalBusiness/src/test/java/ar/com/fdv/rentalBusiness/domainModel/RentalCompanyTest.java
+++ rentalBusiness/src/test/java/ar/com/fdv/rentalBusiness/domainModel/RentalCompanyTest.java
@@ -1,5 +1,6 @@
 package ar.com.fdv.rentalBusiness.domainModel;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +8,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import ar.com.fdv.rentalBusiness.businessException.BadRequestException;
+import ar.com.fdv.rentalBusiness.businessException.NotAvailableBikesException;
 import ar.com.fdv.rentalBusiness.dto.CustomerOrder;
+import ar.com.fdv.rentalBusiness.rentalStrategy.RentalByDayStrategy;
+import ar.com.fdv.rentalBusiness.rentalStrategy.RentalByHourStrategy;
+import ar.com.fdv.rentalBusiness.rentalStrategy.RentalByWeekStrategy;
+import ar.com.fdv.rentalBusiness.rentalStrategy.RentalStrategy;
 
 public class RentalCompanyTest {
 
@@ -16,34 +22,111 @@ public class RentalCompanyTest {
 	private Bike bike3 = null;
 	private Customer customer1 = null;
 	private Customer customer2 = null;
+	private CustomerOrder customerOrder1 = null;
+	private CustomerOrder customerOrder2 = null;
+	private CustomerOrder customerOrder3 = null;
+	private CustomerOrder customerOrder4 = null;
+	private CustomerOrder customerOrder5 = null;
+	private CustomerOrder customerOrder6 = null;
+	private RentalStrategy rentalByHourStrategy = null; 
+	private RentalStrategy rentalByDayStrategy = null; 
+	private RentalStrategy rentalByWeekStrategy = null; 
 	private RentalCompany rentalCompany = null; 
+	List<Bike> rentedBikes = new ArrayList<Bike>();
+	List<Bike> availableBikes = new ArrayList<Bike>();
+	List<CustomerOrder> customerOrders1 = new ArrayList<CustomerOrder>();
+	List<CustomerOrder> customerOrders2 = new ArrayList<CustomerOrder>();
+	List<CustomerOrder> customerOrders3 = new ArrayList<CustomerOrder>();
 	
 	@Before
 	public void initialize(){
-		List<Bike> rentedBikes = new ArrayList<Bike>();
-		List<Bike> availableBikes = new ArrayList<Bike>();
-		
+		initializeBikes();
+		initializeListsOfBikes();
+		initializeCustomers();
+		initializeRentalStrategies();
+		initializeCustomerOrders();
+		initializeListOfCustomerOrders();
+		initializeRentalCompany(); 
+	}
+	
+	private void initializeBikes() {
 		bike1 = new Bike(1);
 		bike2 = new Bike(2);
 		bike3 = new Bike(3);
-		
+	}
+	
+	private void initializeListsOfBikes() {
 		rentedBikes.add(bike1);
 		availableBikes.add(bike2);
 		availableBikes.add(bike3);
-		
+	}
+
+	private void initializeCustomers() {
 		customer1 = new Customer(100F, null);
 		customer2 = new Customer(10F, rentedBikes);
-		
-		rentalCompany = new RentalCompany(1000F, availableBikes); 
+	}
+
+	private void initializeRentalStrategies() {
+		rentalByHourStrategy = new RentalByHourStrategy();
+		rentalByDayStrategy = new RentalByDayStrategy();
+		rentalByWeekStrategy = new RentalByWeekStrategy();
+	}
+
+	private void initializeCustomerOrders() {
+		customerOrder1 = new CustomerOrder(rentalByHourStrategy, 1);
+		customerOrder2 = new CustomerOrder(rentalByDayStrategy, 2);
+		customerOrder3 = new CustomerOrder(rentalByWeekStrategy, 3);
+		customerOrder4 = new CustomerOrder(rentalByHourStrategy, 4);
+		customerOrder5 = new CustomerOrder(rentalByDayStrategy, 5);
+		customerOrder6 = new CustomerOrder(rentalByWeekStrategy, 6);
+	}
+
+	private void initializeListOfCustomerOrders() {
+		customerOrders1.add(customerOrder1);
+		customerOrders2.add(customerOrder1);
+		customerOrders2.add(customerOrder2);
+		customerOrders2.add(customerOrder3);
+		customerOrders3.add(customerOrder1);
+		customerOrders3.add(customerOrder2);
+		customerOrders3.add(customerOrder3);
+		customerOrders3.add(customerOrder4);
+		customerOrders3.add(customerOrder5);
+		customerOrders3.add(customerOrder6);
+	}
+
+	private void initializeRentalCompany() {
+		rentalCompany = new RentalCompany(1000F, availableBikes);
 	}
 	
 	@Test(expected=BadRequestException.class)
-	public void testNullCustomerOrder() throws BadRequestException{
+	public void testNullCustomerOrders() throws BadRequestException, NotAvailableBikesException, IOException{
 		rentalCompany.rent(null);
 	}
 	
 	@Test(expected=BadRequestException.class)
-	public void testEmptyCustomerOrder() throws BadRequestException{
+	public void testEmptyCustomerOrders() throws BadRequestException, NotAvailableBikesException, IOException{
 		rentalCompany.rent(new ArrayList<CustomerOrder>());
+	}
+
+	@Test(expected=BadRequestException.class)
+	public void testRentalStrategyIsNull() throws BadRequestException, NotAvailableBikesException, IOException{
+		customerOrder1 = new CustomerOrder(null, 1);
+		List<CustomerOrder> customerOrders = new ArrayList<CustomerOrder>();
+		customerOrders.add(customerOrder1);
+		rentalCompany.rent(customerOrders);
+	}
+
+	@Test(expected=BadRequestException.class)
+	public void testTimeQuantityIsNull() throws BadRequestException, NotAvailableBikesException, IOException{
+		customerOrder1 = new CustomerOrder(rentalByDayStrategy, null);
+		List<CustomerOrder> customerOrders = new ArrayList<CustomerOrder>();
+		customerOrders.add(customerOrder1);
+		rentalCompany.rent(customerOrders);
+	}
+	
+	@Test(expected=NotAvailableBikesException.class)
+	public void testNotAvailableBikes() throws BadRequestException, NotAvailableBikesException, IOException{
+		rentalCompany.setAvailableBikes(new ArrayList<Bike>());
+		rentalCompany.rent(customerOrders1);
 	}
 }
